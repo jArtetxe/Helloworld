b0ce7ec163cb74e1445f47540d7c4d38fda19936
jArtetxe <j.artetxe@opendeusto.es>
2024-02-23 08:51:41 +0100

    namespace

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class HelloWorld : MonoBehaviour
-{
-    Enemy e;
-    // Start is called before the first frame update
-    void Start()
+namespace Game{
+    public class HelloWorld : MonoBehaviour
     {
-        Debug.Log("Hello World");
-        e = new Enemy("Bob");
-        Player p = new Player("Pepe");
-    }
+        Enemy e;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hello World");
+            e = new Enemy("Bob");
+            Player p = new Player("Pepe");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Nombre: " + e.name + ", Vida: " + e.HealthPoints);
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("Nombre: " + e.name + ", Vida: " + e.HealthPoints);
+        }
     }
 }
